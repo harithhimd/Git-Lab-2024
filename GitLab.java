fb55f52bd7adfd0df2d9e21d99e4a40ef8eb1ebd
harhar24 <143854094+harithhimd@users.noreply.github.com>
2024-02-07 11:16:52 -0600

    Helper Function Added

--- GitLab.java
+++ GitLab.java
@@ -1,5 +1,8 @@
 public class GitLab{
     public static void main(String[] args) {
         System.out.println("This is a lesson on git");
-        }
+        GitLabHelper helper = new GitLabHelper("Harley", "Harith");
+
+        helper.PrintWelcomeMessage();
+    }
 }
